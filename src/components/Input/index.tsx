a112616ad8169340fb9502b89bf149185f9d14ff
thyago608 <thyagoribeiro608@gmail.com>
2022-08-22 08:19:05 -0400

    Created Component Input and Styled

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import { InputHTMLAttributes, ForwardRefRenderFunction, forwardRef } from "react";
+import "./styles.css";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    label: string;
+}
+
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ label, name, ...rest }, ref) => {
+    return (
+        <div className="input_container">
+            <label htmlFor={name}>{label}</label>
+            <input id={name}{...rest} ref={ref} />
+        </div>
+    )
+}
+
+export const Input = forwardRef(InputBase);
